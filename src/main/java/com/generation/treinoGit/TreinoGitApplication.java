ba682e8b4ac7d89d5ad9a5ec9bdcb4170231fc17
kaiquesilvadev <j.kaique1570@gmail.com>
2023-05-19 12:42:39 -0300

    treinando com o git para melhorar minhas hablidades

---
+++ src/main/java/com/generation/treinoGit/TreinoGitApplication.java
@@ -0,0 +1,13 @@
+package com.generation.treinoGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinoGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinoGitApplication.class, args);
+	}
+
+}
